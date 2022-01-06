67bae78c8bdfac671b18d34028291cbfe6c0e391
Ax-Sch <axel.schmidt1@rwth-aachen.de>
2022-01-06 14:34:33 +0100

    modified bash script combine_all_proteins_to_one_file to just use files with TRUE in the name

--- scripts/combine_all_proteins_to_one_file.sh
+++ scripts/combine_all_proteins_to_one_file.sh
@@ -12,25 +12,27 @@ mkdir -p $out_dir"/tmp"
 zcat $first_file | head -n1 > $out_dir"/tmp/header.csv"
 zcat_sting=""
 
+# loop over directory content and extract collect non empty files with TRUE in filename 
 for file in $(ls $director_combined_files)
 do
+if [[ $file == *"TRUE"* ]]; then
 f_size=$(stat --printf="%s" $director_combined_files"/"$file)
 if [ $f_size -gt 100 ]
 then
 zcat_string+=" ${director_combined_files}/${file}"
 fi
+fi
 done
 
-relevant_cols=$(zcat data/validation_set/validation_set_w_AlphScore.csv.gz | awk -v RS=',' '/^b_factor$/{print NR} /^SOLVENT_ACCESSIBILITY_core$/{print NR} /^Uniprot_acc_split$/{print NR} /^train_ds$/{print NR} /AlphScore/{print NR; exit}' | tr "\n" ",")"2,3,4,5,6,7,8,9,10"
-
-echo "using columns:"
-echo $relevant_cols
+#relevant_cols=$(zcat data/validation_set/validation_set_w_AlphScore.csv.gz | awk -v RS=',' '/^b_factor$/{print NR} /^SOLVENT_ACCESSIBILITY_core$/{print NR} /^Uniprot_acc_split$/{print NR} /^train_ds$/{print NR} /AlphScore/{print NR; exit}' | tr "\n" ",")"2,3,4,5,6,7,8,9,10"
 
 
 echo $zcat_string
 
-cat $out_dir"/tmp/header.csv" | cut -f $relevant_cols -d"," > $out_dir"/tmp/header_short.csv"
-zcat $zcat_string  | cut -f $relevant_cols -d"," | grep -v "chr,pos(1-based)," | sort -t, -T $out_dir"/tmp/" -k2,2 -k3,3n - | cat $out_dir"/tmp/header_short.csv" - | bgzip -c > $out_dir"/all_possible_values_concat_small.csv.gz"
+cat $out_dir"/tmp/header.csv" > $out_dir"/tmp/header_short.csv"
+
+zcat $zcat_string  | grep -v "chr,pos(1-based)," | sort -t, -T $out_dir"/tmp/" -k2,2 -k3,3n - | \
+cat $out_dir"/tmp/header_short.csv" - | bgzip -c > $out_dir"/all_possible_values_concat.csv.gz"
 
 
 #zcat $zcat_string | grep -v "chr,pos(1-based),ref,alt,aaref,aaalt," | sort -t, -T $out_dir"/tmp/" -k2,2 -k3,3n - | cat $out_dir"/tmp/header.csv" - | bgzip -c > $out_dir"/all_possible_values_concat.csv.gz"
