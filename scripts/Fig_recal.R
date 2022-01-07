6f335c18d3a28741ed98d9562b256b1afcafe838
KarolaM <95022062+KarolaM@users.noreply.github.com>
2022-01-07 11:15:35 +0100

    Automatical adjustment of x axes

--- scripts/Fig_recal.R
+++ scripts/Fig_recal.R
@@ -42,6 +42,9 @@ plot_pre<- ggplot(from_AS_pre,aes(x=reorder(from_AS, -frac), y=frac)) +
 
 ggsave("barplot_preprocessed.pdf", width=200, height=100, units="mm", dpi=300)
 
+order_AA_pre<- from_AS_pre[order(-from_AS_pre$frac),]
+order_AA_pre<-order_AA_pre$from_AS
+
 ####recalibrated
 counts_recal<- variants_recal %>%
   filter(!(pure_cv18_to_21_gene==TRUE) & gnomadSet==TRUE)%>%
@@ -54,10 +57,12 @@ from_AS_recal<-variants_recal %>%
   select(from_AS, outcome) %>%
   summarise(frac=mean(outcome))
 
+from_AS_recal<-from_AS_recal[match(order_AA_pre, from_AS_recal$from_AS),]
+from_AS_recal$from_AS<-factor(from_AS_recal$from_AS, levels=from_AS_recal$from_AS)
 
-plot_recal<-ggplot(from_AS_recal,aes(x=factor(from_AS,level=c("TYR","PHE","TRP","LYS","LEU","ASP","HIS","CYS","MET","ILE","GLU","GLN","SER","ASN","GLY","PRO","THR","ALA","VAL","ARG")), y=frac)) + 
+plot_recal<-ggplot(from_AS_recal,aes(x=from_AS, y=frac)) + 
   geom_bar(stat = "identity")+
-  geom_text(aes(x=reorder(from_AS, -frac), y=frac, label=counts_recal$n),
+  geom_text(aes(x=from_AS, y=frac, label=counts_recal$n),
             position = position_dodge(width = 1),
             vjust = -0.5, 
             size = 2.5, colour = "black", check_overlap = TRUE)+
